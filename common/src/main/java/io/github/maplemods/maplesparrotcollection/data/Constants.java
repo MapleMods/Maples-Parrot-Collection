08b86c47853c8c1ed33ce29ff041372854cb1eca
MapleMods <250133667+MapleMods@users.noreply.github.com>
2026-05-16 11:54:25 +0200

    Maple's Parrot Collection 1.0 for 1.21.11

---
+++ common/src/main/java/io/github/maplemods/maplesparrotcollection/data/Constants.java
@@ -0,0 +1,11 @@
+package io.github.maplemods.maplesparrotcollection.data;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+	public static final String MOD_ID = "maplesparrotcollection";
+	public static final String MOD_NAME = "Maple's Parrot Collection";
+	public static final String MOD_VERSION = "1.0";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
